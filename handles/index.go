16d8cb3e8268f7a70ac6107bb2c80bd7e371d997
ppz <372301467@qq.com>
2023-10-07 04:03:01 +0000

    fix: go 是简单的，也是繁琐的；不是好玩的，放弃好玩改造

--- handles/index.go
+++ handles/index.go
@@ -2,20 +2,19 @@ package handles
 
 import (
 	"_/context"
-	"_/utils/map2"
 	"fmt"
 	"net/http"
 )
 
 type _handle func(context.Request)
-type _restful map[string]_handle      // request.method => handle
-type _all_handles map[string]_restful // request.path => restful
+type _handles map[string]_handle      // request.method => handle
+type _all_handles map[string]_handles // request.path => _handles
 
 const GET = http.MethodGet
 const POST = http.MethodPost
 
 func Collect() {
-	handles := _all_handles{
+	all_handles := _all_handles{
 		"/": {
 			GET: func(ctx context.Request) {
 				if ctx.Req.URL.Path == "/" {
@@ -35,17 +34,20 @@ func Collect() {
 
 	app_context := context.New_app()
 
-	map2.Arm(handles).Each(func(restful _restful, path string) {
-		fmt.Println("route:", path, restful)
+	add_handle := func(path string, handles _handles) {
+		fmt.Println("route:", path, handles)
 		http.HandleFunc(path, func(res http.ResponseWriter, req *http.Request) {
 			fmt.Printf("[received request: %s %s][matched: %s]\n", req.Method, req.URL.Path, path)
-			handle, ok := restful[req.Method]
+			handle, ok := handles[req.Method]
 			if !ok {
 				handle = handle_404
 			}
 			handle(context.New_request(res, req, app_context))
 		})
-	})
+	}
+	for path, handles := range all_handles {
+		add_handle(path, handles)
+	}
 }
 
 func handle_404(ctx context.Request) {
