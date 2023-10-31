daf86009e8c1f06036bba3cc0ff4f2f60f24e566
ppz <372301467@qq.com>
2023-10-31 10:46:06 +0800

    feat: hooks

---
+++ front/src/common/hooks.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react'
+
+export
+const useMount = on_mount =>
+  useEffect(
+    () => on_mount(),
+    [],
+  )
+
+/** useMount and watch */
+export
+const useMount_w = (watch, cb) =>
+  useEffect(
+    () => cb(),
+    watch
+  )
+
+export
+const useData = ({ get, default_value, watch = []}) => {
+  const [val, set_val] = useState(default_value)
+  useMount_w(
+    watch,
+    () => get().then(set_val),
+  )
+  return val
+}
