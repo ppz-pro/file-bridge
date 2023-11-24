678a1b670c7777e0e24e2a70ba023fe725ee5a08
ppz <372301467@qq.com>
2023-11-24 11:18:54 +0800

    feat: websocket mng

--- handles/a.provider.go
+++ handles/a.provider.go
@@ -7,10 +7,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
 func collect_provider(engine *gin.Engine) {
 	group := engine.Group("/provider")
+
 	group.GET("/jwt", func(c *gin.Context) {
 		token := jwt.NewWithClaims(
 			jwt.SigningMethodHS256,
@@ -29,4 +31,28 @@ func collect_provider(engine *gin.Engine) {
 			respond_json_error(c, ERR_CODE_JWT_SIGNING)
 		}
 	})
+
+	provider_map := mng_connections(group)
+}
+
+type request_file_from_downloader func(target_path string)
+type provider_map map[string]request_file_from_downloader
+
+func mng_connections(group *gin.RouterGroup) provider_map {
+	provider_map := make(provider_map)
+
+	upgrade_ws := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	group.GET("/conn", func(c *gin.Context) {
+		conn, err := upgrade_ws.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			respond_json_error(c, ERR_CODE_WS_UPGRADE)
+			return
+		}
+		conn.Close()
+	})
+
+	return provider_map
 }
--- handles/z.consts.go
+++ handles/z.consts.go
@@ -1,10 +1,13 @@
 package handles
 
 const (
-	NO_ERR                  = 0
-	ERR_CODE_REQUEST_JSON   = 1001
+	NO_ERR = 0
+
+	ERR_CODE_REQUEST_JSON   = 1001 // 1000+ common error
 	ERR_CODE_REQUEST_QUERY  = 1002
 	ERR_CODE_REQUEST_PARAMS = 1003
 
-	ERR_CODE_JWT_SIGNING = 5001
+	ERR_CODE_WS_UPGRADE = 2001 // 2000+ websocket error
+
+	ERR_CODE_JWT_SIGNING = 5001 // 5000+ business error
 )
